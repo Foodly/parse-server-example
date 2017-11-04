064c086662a6cfedea186efe9dde043146c4f3bb
Droan Rishi <drishi@hearsaycorp.com>
2017-11-04 15:38:46 -0700

    Fix connecting to parse server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var https = require('https');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -21,6 +22,8 @@ var pushConfig = {'ios': { token: {
   production: false // set explicitly
  }
 };
+
+Parse.serverUrl = process.env.SERVER_URL;
 
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
