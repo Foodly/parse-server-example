e72f13920f20ee4bf213ec0673740ad74ddd8d85
Droan Rishi <drishi@hearsaycorp.com>
2017-11-06 21:02:52 -0800

    Add job

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,20 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+Parse.Cloud.define("ingredientReminderJob", function(request, response) {
+  var query = new Parse.Query("Ingredient");
+  query.find({
+    success: function(ingredients) {
+      for(var i = 0; i < users.length; ++i) {
+        console.log(ingredients[i].get("name"));
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    },
+  });
+});
+
 Parse.Cloud.define("iosPushTest", function(request, response) {
   var params = request.params;
   var channel = params.channel;
